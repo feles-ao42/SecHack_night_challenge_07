d0b84fadca1e18c3dd45863b2861cf476c9fb30d
feles-ao42 <ao.42ao.42@gmail.com>
2022-07-10 09:28:18 +0900

    現状push

--- python/main.py
+++ python/main.py
@@ -8,10 +8,10 @@ app.debug = True
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
-url1 = "http://127.0.0.1:5555/fizzbuzz/while"
-url2 = "http://127.0.0.1:5555/fizzbuzz/while"
-url3 = "http://127.0.0.1:5555/fizzbuzz/while"
-url4 = "http://127.0.0.1:5555/fizzbuzz/while"
+url1 = "http://192.168.52.105:5555/fizzbuzz/while"  # 古本
+url2 = "http://192.168.52.107:5555/fizzbuzz/while"  # 武藤さん
+url3 = "http://192.168.52.106:5555/fizzbuzz/while"  # 田村さん
+url4 = "http://127.0.0.1:5555/fizzbuzz/while"  # さきさん
 
 
 @app.after_request
